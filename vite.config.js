41bf69c5cd7c22cc6aa827e466b0a39a13c80021
七彩 <wangriqiao@cai-inc.com>
2025-10-30 20:12:44 +0800

    feat: 触发

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-
 export default defineConfig({
   plugins: [react()],
   base: './',  // 添加这一行以支持 GitHub Pages
